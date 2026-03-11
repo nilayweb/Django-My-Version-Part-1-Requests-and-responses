eedc29c63b281e4b2193dd895563052ebfbb22a9
Nilay Özçelik <nilayozceli649@gmail.com>
2026-03-11 11:34:54 +0300

    Bölüm tamamlandı: Görünümler güncellendi, statik CSS eklendi ve hatalar giderildi

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 
 def index(request):
-    # En yeni sorular önce gelecek şekilde sırala
     latest_questions = Question.objects.all().order_by('-pub_date')
     context = {'latest_questions': latest_questions}
     return render(request, 'polls/index.html', context)
@@ -12,3 +13,27 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "Bir seçenek seçmediniz.",
+        })
+
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
