54cc83c67af6c0f8f6682caea19d940d810b60a0
Nilay Özçelik <nilayozceli649@gmail.com>
2026-03-11 11:41:17 +0300

    Proje tamamlandı: Bölüm 8 - Django Debug Toolbar eklendi

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+    'debug_toolbar',  # <-- Bunu ekledik
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware', # <-- Bunu ekledik
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -116,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
